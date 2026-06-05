cea02579f350ffc63820b1a7a703768aecfc73c0
manu-jimenez6 <manuela.jimenezm@udea.edu.co>
2026-06-04 22:28:50 -0500

    js login funcional conectado al back de luisa

---
+++ js/login.js
@@ -0,0 +1,56 @@
+const form = document.getElementById("form-login");
+
+form.addEventListener("submit", async (e) => {
+
+    e.preventDefault();
+
+    const correo = document.getElementById("email").value;
+
+    const contrasena =
+        document.getElementById("contrasena-login").value;
+
+    try {
+
+        const respuesta = await fetch(
+            "http://localhost:8080/api/auth/login",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    correo,
+                    contrasena
+                })
+            }
+        );
+
+        const data = await respuesta.json();
+
+        console.log("STATUS:", respuesta.status);
+        console.log("DATA:", data);
+
+        if (!respuesta.ok) {
+            throw new Error(data.mensaje || "Credenciales incorrectas");
+        }
+
+        localStorage.setItem(
+            "usuario",
+            JSON.stringify(data)
+        );
+
+        alert("Login correcto");
+
+        if (data.rol === "ADMIN") {
+            window.location.href = "panel-admin.html";
+        } else {
+            window.location.href = "dashboard-usuario.html";
+        }
+
+    } catch(error) {
+
+        console.error(error);
+
+        alert("Error al iniciar sesión");
+    }
+});
